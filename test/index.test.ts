2543ffeccefd836168a5da1d15cb09832fe6f173
SmilingXinyi <smilingxinyi@gmail.com>
2019-12-05 20:45:21 +0800

    eslint & demo

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
-import sum from "../src";
+import sum from '../src';
 
-describe("sdk-template", () => {
-    test("invoke crypto.getRandomValues should return ten values of UInt8Array", () => {
+describe('sdk-template', () => {
+    test('invoke crypto.getRandomValues should return ten values of UInt8Array', () => {
         let t = new Uint8Array(10);
         expect(crypto.getRandomValues(t)).toHaveLength(10);
     });
